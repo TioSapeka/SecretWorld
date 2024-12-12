f90288d03bb9e4b7092b6a6bb5e3a4c101924859
Peterson Costa <petinho.costa@hotmail.com>
2024-12-12 10:23:49 -0300

    feat: secretworld

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Corsa", "Celta", "Jeep", "Ferrari", "Golf"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    paises: ["Brasil", "Argentina", "França", "Australia"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  };
